137e3f170be5cdc84f292dc262d28e3cda4e6ce0
Suman maharana <127583084+Sumanmaharana99@users.noreply.github.com>
2025-03-31 13:13:20 +0530

    Added Comments

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,7 +4,7 @@ import userModel from "../models/userModel.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res) => {
-  try {
+  try { //Creating and saving new user to database
     const newUser = new userModel(req.body);
     await newUser.save();
     res.status(201).json({ message: "User registered successfully!" });
