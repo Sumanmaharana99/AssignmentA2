e8ede6f28080c50d56e40e6f2c4d65d66e744369
Suman maharana <127583084+Sumanmaharana99@users.noreply.github.com>
2025-03-31 13:09:15 +0530

    Checking

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,21 +1,14 @@
 import express from "express";
-import userModel from "../models/userModel.js"; // Import the User model
+import userModel from "../models/userModel.js"; 
 
 const userRouter = express.Router();
 
 userRouter.post("/register", async (req, res) => {
   try {
-    
-    // Create a new user with data from the request body
     const newUser = new userModel(req.body);
-
-    // Save the user to MongoDB
     await newUser.save();
-
-    // Send a success response
     res.status(201).json({ message: "User registered successfully!" });
   } catch (err) {
-    // Handle errors (e.g., duplicate email, missing fields)
     res.status(500).json({ error: err.message });
   }
 });
